f1ab08ac296d2d6180d79811a310a42abe4b5cbb
Israt Jahan <israt5227@gmail.com>
2026-07-20 23:44:44 +0600

    fixed friendContex

--- src/context/FriendContext.jsx
+++ src/context/FriendContext.jsx
@@ -9,22 +9,9 @@ const FriendProvider = ({children}) => {
    const [checkIns, setCheckIn] = useState([]);
    
    const handleCall = (friendDetailsShow) => {
-    // const isAlreadyExisted = callInfos.find(callInfo => callInfo.id == friendDetailsShow.id)
-
-    // if(isAlreadyExisted){
-    //   toast.error('this book already exist');
-    // }
-    // else{
-    //   setCallInfos([...callInfos, friendDetailsShow])
-    //   toast.success(`${friendDetailsShow.name} is added to list`)
-    // }
-    ////////////////////////////////
-
-    // setCallInfos([...callInfos, friendDetailsShow])
-    
     
 
-   const newFriendDetailsShow = {...friendDetailsShow, type: <Phone />};
+   const newFriendDetailsShow = {...friendDetailsShow, type: 'Call', icon: <Phone />};
    if(newFriendDetailsShow){
      setCheckIn([...checkIns, newFriendDetailsShow])
     toast.success(`Call with ${newFriendDetailsShow.name}!`)
@@ -33,7 +20,7 @@ const FriendProvider = ({children}) => {
   }
 
   const handleText = (friendDetailsShow) => {
-   const newFriendDetailsShow =  {...friendDetailsShow, type: <MessageSquareText  />};
+   const newFriendDetailsShow =  {...friendDetailsShow, type: 'Text', icon: <MessageSquareText  />};
    if(newFriendDetailsShow){
      setCheckIn([...checkIns, newFriendDetailsShow])
       toast.success(`Text with ${newFriendDetailsShow.name}!`)
@@ -41,7 +28,7 @@ const FriendProvider = ({children}) => {
   }
   
   const handleVideo = (friendDetailsShow) => {
-  const newFriendDetailsShow =  {...friendDetailsShow, type: <SquarePlay  />};
+  const newFriendDetailsShow =  {...friendDetailsShow, type: 'Video', icon: <SquarePlay  />};
   if(newFriendDetailsShow){
     setCheckIn([...checkIns, newFriendDetailsShow])
     toast.success(`Video with ${newFriendDetailsShow.name}!`)
--- src/features/components/TimelineCart.jsx
+++ src/features/components/TimelineCart.jsx
@@ -1,39 +1,92 @@
-import { MessageSquareText, Phone, SquarePlay } from 'lucide-react';
-import { useState } from 'react';
+import { MessageSquareText, Phone, SquarePlay } from "lucide-react";
+import { useEffect, useState } from "react";
 
-const TimelineCart = ({callInfo}) => {
-
-
-    const formattedDate = new Intl.DateTimeFormat('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
+const TimelineCart = ({ callInfo }) => {
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   }).format(new Date());
 
+
+  const [sortingType, setSortingType] = useState('')
+  // console.log(sortingType, 'sorting type')
+  
+  // useEffect(()=> {
+
+  //   if(sortingType){
+  //     if(sortingType === 'call'){
+  //       const sortedCall = 
+  //     }
+  //     else if(sortingType === 'text'){
+
+  //     }
+  //     else{
+
+  //     }
+  //   }
+
+  // }, [sortingType])
+
+
+  const handleFilter = ({callInfo}) =>{
+    if(callInfo.type === 'call'){
+      const callArrays = callInfo.filter((callData => callData.type === 'call'))
+    }
+    else if(callInfo.type === 'text'){
+      const callArrays = callInfo.filter((callData => callData.type === 'text'))
+    }
+    else{
+      const callArrays = callInfo.filter((callData => callData.type === 'video'))
+    }
+  }
+
+
+
   return (
-    <div className="w-full container mx-auto bg-white rounded-xl border border-slate-100 shadow-sm p-4 sm:p-5 flex items-center gap-4 transition-all duration-200 hover:shadow-md mb-4">
-      
-
-     {callInfo.type}
-
-      {/* Content Text Container */}
-      <div className="flex flex-col">
-        {/* Header Row */}
-        <div className="flex flex-wrap items-baseline gap-1.5">
-          <span className="text-base sm:text-lg font-bold text-[#1e463a] tracking-tight">
-           {/* {callInfo} */}
-          </span>
-          <span className="text-sm sm:text-base text-slate-500">
-            with {callInfo.name}
-          </span>
+    <div className="container mx-auto">
+      <div className="dropdown dropdown-bottom">
+        <div tabIndex={0} role="button" className="btn m-1 text-[#64748B] text-[18px] font-normal">
+          Filter Timeline   ⬇️
         </div>
-
-        {/* Date Time Stamp */}
-        <span className="text-xs sm:text-sm text-slate-400 mt-0.5 font-medium">
-          {formattedDate}
-        </span>
+        <ul
+          tabIndex="-1"
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li onClick={()=> handleFilter(callInfo)}>
+            <a>Call</a>
+          </li>
+          <li onClick={()=> handleFilter(callInfo)}>
+            <a>Text</a>
+          </li>
+          <li onClick={()=> handleFilter(callInfo)}>
+            <a>Video</a>
+          </li>
+        </ul>
       </div>
 
+
+      <div className="w-full container mx-auto bg-white rounded-xl border border-slate-100 shadow-sm p-4 sm:p-5 flex items-center gap-4 transition-all duration-200 hover:shadow-md mb-4">
+        {callInfo.icon}
+
+        {/* Content Text Container */}
+        <div className="flex flex-col">
+          {/* Header Row */}
+          <div className="flex flex-wrap items-baseline gap-1.5">
+            <span className="text-base sm:text-lg font-bold text-[#1e463a] tracking-tight">
+              {callInfo.type}
+            </span>
+            <span className="text-sm sm:text-base text-slate-500">
+              with {callInfo.name}
+            </span>
+          </div>
+
+          {/* Date Time Stamp */}
+          <span className="text-xs sm:text-sm text-slate-400 mt-0.5 font-medium">
+            {formattedDate}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
